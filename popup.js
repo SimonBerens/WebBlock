a347bd227276120b83837ba6abf1c3a60f295075
SimonBerens <sberensnyc@gmail.com>
2020-06-20 17:59:35 -0400

    Add options button

--- popup.js
+++ popup.js
@@ -7,3 +7,8 @@ document.getElementById("start_blocking")
             blockTab(tabs[0]);
         })
     })
+
+document.getElementById("go_to_options")
+    .addEventListener("click", () => {
+        chrome.tabs.create({url: chrome.extension.getURL("options.html")});
+    })
