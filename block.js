ac8315a70372b2ee140193ad44277bea702e87d6
SimonBerens <sberensnyc@gmail.com>
2020-06-19 20:16:27 -0400

    Block sites on block list

--- block.js
+++ block.js
@@ -1,6 +1,6 @@
 export const blockTab = tab => {
-    chrome.storage.sync.get({blocking: true}, res => {
-        if (res.blocking)
+    chrome.storage.sync.get({blocking: true, blocked_sites: []}, res => {
+        if (res.blocking && res.blocked_sites.some(site => tab.url.indexOf(site) === 0))
             alert(tab.url);
     })
 }
