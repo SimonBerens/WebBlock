ba37399041af49dcf7c179600371d7bfe4dfbff0
SimonBerens <sberensnyc@gmail.com>
2020-06-19 20:40:33 -0400

    Implement redirect blocking

--- block.js
+++ block.js
@@ -1,6 +1,8 @@
 export const blockTab = tab => {
     chrome.storage.sync.get({blocking: true, blocked_sites: []}, res => {
-        if (res.blocking && res.blocked_sites.some(site => tab.url.indexOf(site) === 0))
-            alert(tab.url);
+        if (res.blocking && res.blocked_sites.some(site => tab.url.indexOf(site) === 0)) {
+            const absURL = chrome.extension.getURL("/images/thanosfailure.jpg");
+            chrome.tabs.update(tab.id, {url: absURL});
+        }
     })
 }
