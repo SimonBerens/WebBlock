9373c7f6f26cf3e648af301a0c93cc4a92ab1737
SimonBerens <sberensnyc@gmail.com>
2020-06-22 19:01:34 -0400

    Make bg color of popup dependent on blocking state

--- popup.js
+++ popup.js
@@ -1,11 +1,18 @@
-import {addOnClick, setBlocking} from "./utils.js";
+import {addOnClick, setBlocking, getFields} from "./utils.js";
 
-addOnClick("start_blocking", () => setBlocking(true));
+const setBlockingAndBgColor = blocking => {
+    setBlocking(blocking);
+    document.body.style.backgroundColor = blocking? "green" : "red";
+};
+
+addOnClick("start_blocking", () => setBlockingAndBgColor(true));
 
 addOnClick("go_to_options", () => chrome.tabs.create({url: chrome.runtime.getURL("options.html")}));
 
 addOnClick("temp_stop_blocking_button", () => {
     const minutes = parseInt(document.getElementById("temp_stop_blocking_input").value);
     chrome.alarms.create({delayInMinutes: minutes});
-    setBlocking(false);
+    setBlockingAndBgColor(false);
 });
+
+getFields({blocking: true}, res => setBlockingAndBgColor(res.blocking));
