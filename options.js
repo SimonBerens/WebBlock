2c9d5eb1dd6734642ce32fa34e6d91b74cc8af7f
SimonBerens <sberensnyc@gmail.com>
2020-06-23 20:21:02 -0400

    Allow user to import urls from csv

--- options.js
+++ options.js
@@ -6,18 +6,21 @@ addOnClick("stop_blocking", () => {
 });
 
 
-const addTextToHtmlListAndSync = (list, text) => {
-    const item = document.createElement("li");
-    item.appendChild(document.createTextNode(text));
-    item.addEventListener("click", () => {
-        getFields({blocked_sites: []}, res => {
-            const new_list = res.blocked_sites;
-            new_list.splice(new_list.indexOf(text), 1);
-            setFields({blocked_sites: new_list});
+const addToHTMLBlockedList = (...sites) => {
+    const list = document.getElementById("blocked_list");
+    for (const site of sites) {
+        const item = document.createElement("li");
+        item.appendChild(document.createTextNode(site));
+        item.addEventListener("click", () => {
+            getFields({blocked_sites: []}, res => {
+                const new_list = res.blocked_sites;
+                new_list.splice(new_list.indexOf(site), 1);
+                setFields({blocked_sites: new_list});
+            });
+            list.removeChild(item);
         });
-        list.removeChild(item);
-    });
-    list.appendChild(item);
+        list.appendChild(item);
+    }
 };
 
 
@@ -27,8 +30,7 @@ addOnClick("add_button", () => getFields({blocked_sites: []},
         const list = res.blocked_sites;
         list.push(new_site);
         setFields({blocked_sites: list});
-        const html_list = document.getElementById("blocked_list");
-        addTextToHtmlListAndSync(html_list, new_site);
+        addToHTMLBlockedList(new_site);
 }));
 
 
@@ -37,9 +39,21 @@ getFields({blocked_sites: [], enable_on_startup: true}, res => {
 
     const list = document.createElement("ul");
     list.id = "blocked_list";
-    for (const site of res.blocked_sites)
-        addTextToHtmlListAndSync(list, site);
     document.getElementById("blocked_list_container").appendChild(list);
+    addToHTMLBlockedList(...res.blocked_sites);
 });
 
 addOnClick("enable_on_startup_checkbox", event => setFields({enable_on_startup: event.target.checked}));
+
+document.getElementById("import_from_csv").addEventListener("change", async event => {
+    const csv_str = await event.target.files[0].text();
+    const lines = csv_str.split(/\r\n|\n/);
+    const urls = [];
+    for (const line of lines)
+        urls.push(...line.split(",").filter(url => url.length !== 0));
+    getFields({blocked_sites: []}, res => {
+        res.blocked_sites.push(...urls);
+        addToHTMLBlockedList(...urls);
+        setFields({blocked_sites: res.blocked_sites});
+    });
+});
