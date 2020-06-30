61b1436bc53a7eed9f1a92842263bfa03a3122cd
SimonBerens <sberensnyc@gmail.com>
2020-06-30 16:47:12 -0400

    Make removing a URL from perm_blocked_sites allow for unblocking of that site

--- block.js
+++ block.js
@@ -6,16 +6,19 @@ export const blockUnblockTab = tab => {
     const isPrefixOfTabURL = isPrefixOfURL(tab.url);
 
     getFields({blocking: true, blocked_sites: [], perm_blocked_sites: [], redirect: ""}, res => {
-        if (res.perm_blocked_sites.some(isPrefixOfTabURL)) {
-            const absURL = res.redirect ? res.redirect : chrome.runtime.getURL("/images/thanosfailure.jpg");
-            chrome.tabs.update(tab.id, {url: absURL});
-        } else if (res.blocking && res.blocked_sites.some(isPrefixOfTabURL)) {
-            const absURL = res.redirect ? res.redirect : chrome.runtime.getURL(`/images/thanosfailure.jpg?dest=${tab.url}`);
+        const in_perm_list = res.perm_blocked_sites.some(isPrefixOfTabURL);
+        if (in_perm_list || (res.blocking && res.blocked_sites.some(isPrefixOfTabURL))) {
+            const absURL = res.redirect ? res.redirect :
+                chrome.runtime.getURL(`/images/thanosfailure.jpg?dest=${tab.url}&perm=${in_perm_list}`);
             chrome.tabs.update(tab.id, {url: absURL});
         } else if (isPrefixOfTabURL(chrome.runtime.getURL(""))) {
             const url_obj = new URL(tab.url);
             const go = url_obj.searchParams.get("dest");
-            if (go && (!res.blocking || !res.blocked_sites.some(isPrefixOfURL(go)))) chrome.tabs.update(tab.id, {url: go});
+            const perm_blocked = url_obj.searchParams.get("perm") === "true";
+            if (!perm_blocked && go &&
+                (!res.blocking || !res.blocked_sites.some(isPrefixOfURL(go))) ||
+                !res.perm_blocked_sites.some(isPrefixOfURL(go)))
+                chrome.tabs.update(tab.id, {url: go});
         }
     });
 };
