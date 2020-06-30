71e665d6f6bcf262dbe1a6355fcdb1f88f6e1d05
SimonBerens <sberensnyc@gmail.com>
2020-06-29 23:24:11 -0400

    Trigger unblock when site is removed from blocked_sites

--- block.js
+++ block.js
@@ -1,19 +1,21 @@
 import {getFields} from "./utils.js";
 
-const isPrefixOfTabURL = tab => (site => tab.url.indexOf(site) === 0);
+const isPrefixOfURL = url => (site => url.indexOf(site) === 0);
 
 export const blockUnblockTab = tab => {
+    const isPrefixOfTabURL = isPrefixOfURL(tab.url);
+
     getFields({blocking: true, blocked_sites: [], perm_blocked_sites: [], redirect: ""}, res => {
-        if (res.perm_blocked_sites.some(isPrefixOfTabURL(tab))) {
+        if (res.perm_blocked_sites.some(isPrefixOfTabURL)) {
             const absURL = res.redirect ? res.redirect : chrome.runtime.getURL("/images/thanosfailure.jpg");
             chrome.tabs.update(tab.id, {url: absURL});
-        } else if (res.blocking && res.blocked_sites.some(isPrefixOfTabURL(tab))) {
+        } else if (res.blocking && res.blocked_sites.some(isPrefixOfTabURL)) {
             const absURL = res.redirect ? res.redirect : chrome.runtime.getURL(`/images/thanosfailure.jpg?dest=${tab.url}`);
             chrome.tabs.update(tab.id, {url: absURL});
-        } else if (!res.blocking && tab.url.indexOf(chrome.runtime.getURL("")) === 0) {
+        } else if (isPrefixOfTabURL(chrome.runtime.getURL(""))) {
             const url_obj = new URL(tab.url);
             const go = url_obj.searchParams.get("dest");
-            if (go) chrome.tabs.update(tab.id, {url: go});
+            if (go && (!res.blocking || !res.blocked_sites.some(isPrefixOfURL(go)))) chrome.tabs.update(tab.id, {url: go});
         }
     });
 };
