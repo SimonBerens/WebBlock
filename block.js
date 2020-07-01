57c74f067589659da73482e28601c55af1c06e47
SimonBerens <sberensnyc@gmail.com>
2020-06-30 20:26:47 -0400

    Fix weird bug

--- block.js
+++ block.js
@@ -1,6 +1,7 @@
 import {getFields} from "./utils.js";
 
 const isPrefixOfURL = url => (site => url.indexOf(site) === 0);
+const DEFAULT_REDIRECT = "/images/thanosfailure.jpg";
 
 export const blockUnblockTab = tab => {
     const isPrefixOfTabURL = isPrefixOfURL(tab.url);
@@ -9,9 +10,9 @@ export const blockUnblockTab = tab => {
         const in_perm_list = res.perm_blocked_sites.some(isPrefixOfTabURL);
         if (in_perm_list || (res.blocking && res.blocked_sites.some(isPrefixOfTabURL))) {
             const absURL = res.redirect ? res.redirect :
-                chrome.runtime.getURL(`/images/thanosfailure.jpg?dest=${tab.url}&perm=${in_perm_list}`);
+                chrome.runtime.getURL(`${DEFAULT_REDIRECT}?dest=${tab.url}&perm=${in_perm_list}`);
             chrome.tabs.update(tab.id, {url: absURL});
-        } else if (isPrefixOfTabURL(chrome.runtime.getURL(""))) {
+        } else if (isPrefixOfTabURL(chrome.runtime.getURL(DEFAULT_REDIRECT))) {
             const url_obj = new URL(tab.url);
             const go = url_obj.searchParams.get("dest");
             const perm_blocked = url_obj.searchParams.get("perm") === "true";
