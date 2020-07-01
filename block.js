6f04d3ea0eb2b86b07a4f78501ae50527c650c26
SimonBerens <sberensnyc@gmail.com>
2020-07-01 14:44:26 -0400

    Infinite reload bug fix

--- block.js
+++ block.js
@@ -1,7 +1,7 @@
 import {getFields} from "./utils.js";
 
 const isPrefixOfURL = url => (site => url.indexOf(site) === 0);
-const DEFAULT_REDIRECT = "/images/thanosfailure.jpg";
+const DEFAULT_REDIRECT = chrome.runtime.getURL("/images/thanosfailure.jpg");
 
 export const blockUnblockTab = tab => {
     const isPrefixOfTabURL = isPrefixOfURL(tab.url);
@@ -10,15 +10,14 @@ export const blockUnblockTab = tab => {
         const in_perm_list = res.perm_blocked_sites.some(isPrefixOfTabURL);
         if (in_perm_list || (res.blocking && res.blocked_sites.some(isPrefixOfTabURL))) {
             const absURL = res.redirect ? res.redirect :
-                chrome.runtime.getURL(`${DEFAULT_REDIRECT}?dest=${tab.url}&perm=${in_perm_list}`);
+                `${DEFAULT_REDIRECT}?dest=${tab.url}&perm=${in_perm_list}`;
             chrome.tabs.update(tab.id, {url: absURL});
-        } else if (isPrefixOfTabURL(chrome.runtime.getURL(DEFAULT_REDIRECT))) {
+        } else if (isPrefixOfTabURL(DEFAULT_REDIRECT)) {
             const url_obj = new URL(tab.url);
             const go = url_obj.searchParams.get("dest");
             const perm_blocked = url_obj.searchParams.get("perm") === "true";
-            if (!perm_blocked && go &&
-                (!res.blocking || !res.blocked_sites.some(isPrefixOfURL(go))) ||
-                !res.perm_blocked_sites.some(isPrefixOfURL(go)))
+            if ((perm_blocked && !res.perm_blocked_sites.some(isPrefixOfURL(go))) ||
+                (!perm_blocked && (!res.blocking || !res.blocked_sites.some(isPrefixOfURL(go)))))
                 chrome.tabs.update(tab.id, {url: go});
         }
     });
